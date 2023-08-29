9ac1e2343346171d216462a1c58839a49c16b2cb
Michael Ihuoma <41088100+michaelihuoma01@users.noreply.github.com>
2023-08-30 00:34:38 +0400

    Update constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -9,4 +9,4 @@ const Color darkColor = Color(0xFF101223);
 const Color lightColor = Color(0xFF22C7DB);
 
 const String serverKey =
-    "key=AAAAi1ikbeo:APA91bEsLB5zqyr1W6UzKgkO9KJFkimhccpGPpRsyGuMAZUBrgJ-PpS95z_uYiWyFh-VxTyI5NRchUgHL6i5qjQuh0tRvrUs2EuMBMzPRhRN4dOeyRGQy-qfR5VtLI8hOwW8kNOgE5HK";
+    "";
